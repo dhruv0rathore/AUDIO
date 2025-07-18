829574c2e0a22e3e8790ec31f60a413fefda91fe
dhruv0rathore <dhruvsinghrathore420@gmail.com>
2025-07-18 23:03:15 +0530

    changing the model again

--- src/voice_synthesizer.py
+++ src/voice_synthesizer.py
@@ -8,7 +8,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 #print("Loading TTS model...")
 device = "cuda" if torch.cuda.is_available() else "cpu"
-tts_model = TTS("tts_models/en/vctk/vits").to(device)
+tts_model = TTS("tts_models/en/styletts2/styletts2").to(device)
 
 # Replace your old synthesizer function with this one
 
